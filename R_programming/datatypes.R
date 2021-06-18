37c493b9ecd9f5af838aed4e42787bd584b09199
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2021-06-18 10:25:18 -0500

    Course

---
+++ R_programming/datatypes.R
@@ -0,0 +1,22 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: oscarhernandezlopez
+# Created on: 18/06/21
+
+getwd() #Get your working directory
+
+#In R everything is an object. It has 5 basic classes of object
+
+#character
+
+#numeric
+
+#integer
+
+#complex number
+
+#logical
+
+#vector (A vector contain objects of the same class)
+
+#list ( Can contain object of different classes, here are included dataframes)
